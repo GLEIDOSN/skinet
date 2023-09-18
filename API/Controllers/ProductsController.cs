65b9bb43a92816f73b03ce93fcdd47f22c234d4f
Gleidson Freitas <fc_gleidson@hotmail.com>
2023-09-18 16:36:19 -0300

    End of section 22

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -28,6 +28,7 @@ namespace API.Controllers
             _mapper = mapper;
         }
 
+        [CachedAttibute(600)]
         [HttpGet]
         public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
             [FromQuery]ProductSpecParams productParams)
@@ -46,6 +47,7 @@ namespace API.Controllers
             return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex, productParams.PageSize, totalItems, data));
         }
 
+        [CachedAttibute(600)]
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
@@ -60,12 +62,14 @@ namespace API.Controllers
             return _mapper.Map<Product, ProductToReturnDto>(product);
         }     
 
+        [CachedAttibute(600)]
         [HttpGet("brands")]   
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
             return Ok(await  _productBrandRepo.ListAllAsync());
         }
 
+        [CachedAttibute(600)]
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
         {
